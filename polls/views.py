b526a3178c40e68b1368098c780ac41019cd2577
sujalp7420 <sujalpatel2726@gmail.com>
2026-01-08 21:54:45 +0530

    version 1.2

--- polls/views.py
+++ polls/views.py
@@ -4,5 +4,5 @@ from django.http import HttpResponse
 
 
 def index(request):
-    # return render(request,'hello.html')
-    return HttpResponse("Hello")
+    return render(request,'hello.html')
+    # return HttpResponse("Hello")
