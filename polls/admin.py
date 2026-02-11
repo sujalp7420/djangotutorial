8bcfdd5176d3a784677c299dbcc665f9e4107f22
sujalp7420 <sujalpatel2726@gmail.com>
2026-02-12 00:11:04 +0530

    form with sqlite

--- polls/admin.py
+++ polls/admin.py
@@ -1,6 +1,19 @@
 from django.contrib import admin
-from .models import Question, Choice
-# Register your models here.
 
-admin.site.register(Question)
-admin.site.register(Choice)
+from .models import Choice, Question
+
+
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {"fields": ["question_text"]}),
+        ("Date information", {"fields": ["pub_date"], "classes": ["collapse"]}),
+    ]
+    inlines = [ChoiceInline]
+
+class ChoiceInline(admin.TabularInline): ...
+admin.site.register(Question, QuestionAdmin)
